ace816ba50f9adba23ebb8f804314dd1864de552
Nikola Invernizzi <56538696+NikolaInvernizzi@users.noreply.github.com>
2026-02-18 22:54:11 +0100

    Update rspack.config.js

--- rspack.config.js
+++ rspack.config.js
@@ -3,10 +3,10 @@ import { defineConfig } from "@rspack/cli";
 export default defineConfig({
 	devtool: false,
 	entry: {
-		main: "./src/circular-timer-card.js",
+		main: "./src/circular-countdown-card.js",
 	},
 	output: {
 		clean: true,
-		filename: "circular-timer-card.js",
+		filename: "circular-countdown-card.js",
 	},
 });
